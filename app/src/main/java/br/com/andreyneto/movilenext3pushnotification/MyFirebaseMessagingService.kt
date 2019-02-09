5bccfcd361499576f0381b283bdd171d904890b3
Andrey Neto <me@andreyneto.com.br>
2019-02-09 14:24:56 -0200

    Update MyFirebaseMessagingService.kt

--- app/src/main/java/br/com/andreyneto/movilenext3pushnotification/MyFirebaseMessagingService.kt
+++ app/src/main/java/br/com/andreyneto/movilenext3pushnotification/MyFirebaseMessagingService.kt
@@ -1,11 +1,39 @@
 package br.com.andreyneto.movilenext3pushnotification
 
-import android.app.Service
-import android.content.Intent
-import android.os.IBinder
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
-class MyFirebaseMessagingService : Service() {
-    override fun onBind(intent: Intent): IBinder? {
-        return null
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    companion object {
+        private const val TAG = "FCM"
     }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        val notification = remoteMessage.notification
+        Log.i(TAG, "Message ID: ${remoteMessage.messageId}")
+        Log.i(TAG, "Data Message: ${remoteMessage.data}")
+        Log.i(TAG, "Notification Message: $notification")
+
+        notification?.let {
+            val title = it.title
+            val body = it.body
+            val data = remoteMessage.data
+            Log.i(TAG, "Notification Title: $title")
+            Log.i(TAG, "Notification body: $body")
+            Log.i(TAG, "Notification data: $data")
+
+            // Criar notificação
+        }
+    }
+
+    override fun onNewToken(token: String) {
+        Log.i(TAG, token)
+        val firebaseMessaging = FirebaseMessaging.getInstance()
+        firebaseMessaging.subscribeToTopic("MAIN")
+        firebaseMessaging.isAutoInitEnabled = true
+    }
+
 }
